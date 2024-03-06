7c5f4cfa0a28a5f8c82428035e5c1d18ac7540bc
DREXCC123 <dmacaspac@csmbataan.ph.education>
2024-03-07 00:26:22 +0800

    Add project files.

---
+++ CC 123/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m, n;
+        Console.Write("Enter the value of m: ");
+        m = Convert.ToInt32(Console.ReadLine());
+
+        if (m > 0)
+            n = 1;
+        else if (m < 0)
+            n = -1;
+        else
+            n = 0;
+
+        Console.WriteLine("The value of n = " + n);
+    }
+}
